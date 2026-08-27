f09a7175873ade46f877bd6b26113983e4265d32
avAs zAde <abas.zade@outlook.com>
2026-08-28 01:20:30 +0330

    feat(user): implement main file

--- services/user-service/cmd/main.go
+++ services/user-service/cmd/main.go
@@ -1,7 +1,75 @@
+// services/user-service/cmd/main.go
+
 package main
 
-import "log"
+import (
+	"context"
+	"log"
+
+	"pkg/auth"
+	"pkg/postgres"
+
+	"user-service/config"
+	"user-service/internal/repository"
+	"user-service/internal/service"
+)
 
 func main() {
+
+	// یک کانتکست پایه برای شروع پروژه
+	ctx := context.Background()
+
+	// بارگذاری اولیه کانفیگ ها و استفاده از آنها در کل پروژه با cfg
+	cfg, err := config.Load()
+
+	// اگر کانفیگ ها لود نشدند لاگ چاپ شده و برنامه بسته میشود
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	// اینجا دیتابیس ایجاد شده و یک Pool میسازد
+	pool, err := postgres.NewPool(ctx, cfg.Postgres.DSN())
+
+	// مجدد اگر دیتابیس وصل نشد کل برنامه بسته میشود
+	if err != nil {
+		log.Fatalf("failed to connect to postgres: %v", err)
+	}
+
+	// بعد از بستن برنامه اتصال هم قطع میشود
+	defer pool.Close()
+
+	// اتصال به ردیس
+	redisClient, err := repository.NewRedisClient(
+		ctx,
+		cfg.Redis.Addr,
+		cfg.Redis.Password,
+		cfg.Redis.DB,
+	)
+
+	// Terminate service
+	if err != nil {
+		log.Fatalf("failed to connect to redis: %v", err)
+	}
+
+	// cleanup
+	defer redisClient.Close()
+
+	// Dependency Injection for database, redis, token and service
+	userRepo := repository.NewUserRepository(pool)
+	refreshTokenRepo := repository.NewRefreshTokenRepository(pool)
+	otpRepo := repository.NewOTPRepository(redisClient)
+
+	tokens := auth.NewTokenManager(cfg.JWT.Secret, cfg.JWT.AccessTokenTTL)
+
+	userService := service.NewUserService(
+		userRepo,
+		otpRepo,
+		refreshTokenRepo,
+		tokens,
+		cfg.JWT.RefreshTokenTTL,
+	)
+
+	_ = userService // هنوز لایه هندلر نوشته نشده
+
 	log.Println("User service started")
 }
