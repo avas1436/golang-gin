6d331d55fb5fbc120cb35c169d9558b7cc3fe29e
avAs zAde <abas.zade@outlook.com>
2026-09-24 23:33:55 +0330

    add consumer provider to rabbitmq module

--- pkg/rabbitmq/module.go
+++ pkg/rabbitmq/module.go
@@ -21,6 +21,15 @@ func NewConnection(
 	return Connect(cfg.URL())
 }
 
+// NewConsumerProvider یک کانال جدید باز کرده و Consumer را ایجاد می‌کند
+func NewConsumerProvider(conn *Connection) (*Consumer, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, err
+	}
+	return NewConsumer(ch)
+}
+
 // RegisterLifecycle مسئول shutdown کردن Connection است.
 func RegisterLifecycle(
 	lc fx.Lifecycle,
@@ -43,6 +52,7 @@ var Module = fx.Module(
 
 	fx.Provide(
 		NewConnection,
+		NewConsumerProvider,
 	),
 
 	fx.Invoke(
